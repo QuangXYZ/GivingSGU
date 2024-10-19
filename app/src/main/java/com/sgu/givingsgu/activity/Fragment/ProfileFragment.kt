40f69e1bec830c1dd31954c7fa65ab29eb6849d6
Quuaann <rjck024vipro@gmail.com>
2024-10-19 14:28:48 +0700

    update

--- app/src/main/java/com/sgu/givingsgu/activity/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/sgu/givingsgu/activity/Fragment/ProfileFragment.kt
@@ -1,14 +1,17 @@
 package com.sgu.givingsgu.activity.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sgu.givingsgu.R
+import com.sgu.givingsgu.activity.LoginActivity
+import com.sgu.givingsgu.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
 
 
     override fun onCreateView(
@@ -16,7 +19,26 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        settingUpListener()
+    }
+
+    private fun settingUpListener() {
+    }
+
+
+    fun init() {
+        // init
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            // Bắt đầu Activity mới
+            startActivity(intent)
+        })
+    }
 }
