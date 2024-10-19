52fffe0e3b44b74685e8647b04ee664d24a04de7
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2024-10-19 13:51:08 +0700

    feature create project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -61,8 +62,9 @@ dependencies {
     implementation ("androidx.fragment:fragment-ktx:1.8.4")
     implementation ("com.google.android.material:material:1.12.0")
     implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-
-
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-storage-ktx:21.0.1")
 
 
 
---
+++ app/src/main/java/com/sgu/givingsgu/adapter/AdminProjectHighLightAdapter.kt
@@ -0,0 +1,81 @@
+package com.sgu.givingsgu.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sgu.givingsgu.activity.ProjectDetailActivity
+import com.sgu.givingsgu.databinding.AdminSingleProjectLayoutBinding
+import com.sgu.givingsgu.model.Project
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.temporal.ChronoUnit
+import java.util.Date
+
+class AdminProjectHighLightAdapter(val projects: MutableList<Project>) :
+    RecyclerView.Adapter<AdminProjectHighLightAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        val binding =
+            AdminSingleProjectLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.adminTitle.text = projects[position].name
+        holder.binding.adminDonateAmount.text = projects[position].currentAmount.toString() + " VND"
+        val percent = (projects[position].currentAmount?.div(projects[position].targetAmount)
+            ?.times(100))
+            ?.toInt()
+        holder.binding.adminDonatePercent.text = percent.toString() + "%"
+        holder.binding.adminLinearProgressIndicator.progress = percent!!
+
+        holder.binding.adminDonateNumber.text =
+            projects[position].numberDonors.toString() + " người đã ủng hộ"
+        holder.binding.projectTime.text =
+            formatTimeDifference(projects[position].startDate!!).toString()
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context, ProjectDetailActivity::class.java)
+//            intent.putExtra("object", projects[position])
+            holder.itemView.context.startActivity(intent)
+        }
+    }
+
+
+    override fun getItemCount(): Int = projects.size
+
+    class ViewHolder(val binding: AdminSingleProjectLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    fun formatTimeDifference(startDate: Date): String {
+        // Chuyển đổi Date sang LocalDateTime
+        val startDateTime = startDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
+        val currentDateTime = LocalDateTime.now() // Ngày và giờ hiện tại
+
+        // Tính số tháng, ngày và giờ
+        val months = ChronoUnit.MONTHS.between(startDateTime, currentDateTime)
+        val days = ChronoUnit.DAYS.between(startDateTime, currentDateTime) % 30 // Số ngày còn lại
+        val hours = ChronoUnit.HOURS.between(startDateTime, currentDateTime) % 24 // Số giờ còn lại
+
+        // Xác định định dạng cho chuỗi đầu ra
+        return when {
+            months > 0 -> "$months tháng trước"
+            days > 0 -> "$days ngày trước"
+            hours > 0 -> "$hours giờ trước"
+            else -> "Vừa mới"
+        }
+    }
+
+}
+
+
